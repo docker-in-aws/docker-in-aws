5476a66285283243e4b7d2e4a57b7147815eb0a6
Justin Menga <justin.menga@gmail.com>
2018-07-17 00:11:10 +1200

    Chapter 6 updates

--- ch6/packer-ecs/scripts/storage.sh
+++ ch6/packer-ecs/scripts/storage.sh
@@ -3,8 +3,7 @@ set -e
 
 echo "### Configuring Docker Volume Storage ###"
 sudo service docker stop
-sudo rm -rf /var/lib/docker/volumes
-sudo mkdir -p /var/lib/docker/volumes
+sudo mkdir -p /data
 sudo mkfs.ext4 -L docker /dev/xvdcy
-echo -e "LABEL=docker\t/var/lib/docker/volumes\t\text4\tdefaults,noatime\t0\t0" | sudo tee -a /etc/fstab
+echo -e "LABEL=docker\t/data\t\text4\tdefaults,noatime\t0\t0" | sudo tee -a /etc/fstab
 sudo mount -a
